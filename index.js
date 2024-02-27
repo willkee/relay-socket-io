5515680059fdef04a1e26cb386a45585e32f1f19
Will Kee <william.w.kee@gmail.com>
2024-02-27 09:14:03 -0700

    added start script

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
 const express = require("express");
 const app = express();
-const http = require("http");
-const { Server } = require("socket.io");
+
+const http = require("http").Server(app);
 const cors = require("cors");
 
-app.use(cors());
-
-const server = http.createServer(app);
-const io = new Server(server, {
+const socketIO = require("socket.io")(http, {
 	cors: { origin: true, methods: ["GET", "POST"] },
 });
 
-io.on("connection", (socket) => {
-	console.log(`a user connected ${socket.id}`);
+app.use(cors());
 
-	socket.on("send_message", (data) => {
-		socket.broadcast.emit("receive_message", data);
+const PORT = 4200;
+
+socketIO.on("connection", (socket) => {
+	console.log(`User: ${socket.id} has connected...`);
+
+	socket.on("disconnect", () => {
+		console.log(`A user has disconnected.`);
 	});
 });
 
-server.listen(4000, () => {
-	console.log("listening on *:4200");
+// const server = http.createServer(app);
+// const io = new Server(server, {
+// 	cors: { origin: true, methods: ["GET", "POST"] },
+// });
+
+// io.on("connection", (socket) => {
+// 	console.log(`a user connected ${socket.id}`);
+
+// 	socket.on("send_message", (data) => {
+// 		socket.broadcast.emit("receive_message", data);
+// 	});
+// });
+
+app.get("/io", (req, res) => {
+	res.send("Hello from Socket.io server!");
+});
+
+http.listen(PORT, () => {
+	console.log(`Socket.io server listening on port ${PORT}...`);
 });
